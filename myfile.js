008b79e450ce041e528a9f8a365811da330ff1ad
Ian Mrak <ianmrak@gmail.com>
2016-07-07 10:22:24 -0700

    changes

--- myfile.js
+++ myfile.js
@@ -1,29 +1,4 @@
 
 
-
-// Aaron might have been here, but Chuck Norris def was here.
-
-
-
-
-
-
-// Ian wasn't here
-
-// Damon was here
-// (ﾉಥ益ಥ）ﾉ﻿ ┻━┻
-
-// ┬──┬﻿ ¯\_(ツ)
-
-// ┻━┻ ︵ヽ(`Д´)ﾉ︵﻿ ┻━┻
-
-// ┻━┻ ︵﻿ ¯\(ツ)/¯ ︵ ┻━┻
-
-// ┬─┬ノ( º _ ºノ)
-
-// (ノಠ益ಠ)ノ彡┻━┻
-
-
-
-
 // Lee is here, maybe
+// changes
